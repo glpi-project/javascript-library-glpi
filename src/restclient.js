75168e3dca99b57b6993ff56fd0252af51a20bec
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-01-15 10:27:43 -0400

    feat(GlpiRestClient): create basic restclient

---
+++ src/restclient.js
@@ -0,0 +1,50 @@
+import prepareRequest from './prepareRequest'
+
+class GlpiRestClient { 
+    constructor (url, appToken = '') {
+        this._url = url
+        this._sessionToken = ''
+        this._appToken = appToken
+    }
+
+    get url () {
+        return this._url
+    }
+
+    set url (url) {
+        this._url = url
+    }
+
+    get sessionToken () {
+        return this._sessionToken
+    }
+
+    set sessionToken (sessionToken) {
+        if (sessionToken) this._sessionToken = sessionToken
+    }
+
+    get appToken () {
+        return this._appToken
+    }
+
+    set appToken (appToken) {
+        if (appToken) this._appToken = appToken
+    }
+
+    _makeRequest (myRequest, responseHandler) {
+        fetch (myRequest)
+            .then((resp) => {            
+                if (resp.headers.get('Content-Type').indexOf("application/json") >= 0) {
+                    responseHandler(resp.json())
+                } else {
+                    responseHandler(resp.text())
+                }
+            }) 
+            .catch((err) => {
+                responseHandler(err)
+            })
+    }
+
+}
+
+export default GlpiRestClient
