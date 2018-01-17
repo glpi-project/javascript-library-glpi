886bf9ce0a32a9a4e970643e0574fe83b2ee6dac
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-01-17 16:06:13 -0400

    ci(build): mount library compilation in develop

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+yarn release -t ''
+export GIT_TAG=$(jq -r ".version" package.json)
+git reset HEAD^1
+
 yarn build 
 
-if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_COMMIT_MESSAGE" != "ci(build): update compiled" ]
+if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_COMMIT_MESSAGE" != *"ci(build): update library to version "* ]
 then
     git checkout "$TRAVIS_BRANCH"
-    git add .
-    git commit -m "ci(build): update compiled"
+    git add lib/
+    git commit -m "ci(build): update library to version ${GIT_TAG}"
     git push origin "$TRAVIS_BRANCH"
 fi
