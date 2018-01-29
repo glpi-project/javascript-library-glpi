1d4e6265e0e4111165912e2d2d048d35d55d6132
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-01-29 10:18:07 -0400

    ci(GlpiRestClient): change Travis variables for those of Circle

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-git checkout "$TRAVIS_BRANCH"
+git checkout "$CIRCLE_BRANCH"
 yarn release -t ''
 GIT_TAG=$(jq -r ".version" package.json)
 git reset HEAD^1
 
 yarn build 
 
-if [ "$TRAVIS_BRANCH" = "develop" ] && [[ "$TRAVIS_COMMIT_MESSAGE" != *"ci(build): update library to version "* ]]
+if [ "$CIRCLE_BRANCH" = "develop" ] && [[ "$GIT_COMMIT_DESC" != *"ci(build): update library to version "* ]]
 then
     git add lib/
     git commit -m "ci(build): update library to version ${GIT_TAG}"
-    git push origin "$TRAVIS_BRANCH"
+    git push origin "$CIRCLE_BRANCH"
 fi
