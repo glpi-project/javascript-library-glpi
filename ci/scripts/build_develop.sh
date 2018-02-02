196517d7794bf04a7629528f40c9292f21d26e13
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-02 18:10:07 -0400

    ci(npm): create prerelease versions
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/build_develop.sh
+++ ci/scripts/build_develop.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 git checkout "$CIRCLE_BRANCH"
-yarn release -t ''
+yarn release -t '' --prerelease beta
 GIT_TAG=$(jq -r ".version" package.json)
-git reset HEAD^1
 
 yarn build 
 
 if [[ "$GIT_COMMIT_DESC" != *"build(npm): update library to version "* ]]
 then
     git add lib/
-    git commit -m "build(npm): update library to version ${GIT_TAG}"
+    git commit -m "build(npm): update library to version ${GIT_TAG}" --amend
     git push origin "$CIRCLE_BRANCH"
     npm publish --tag beta --access public
 fi
