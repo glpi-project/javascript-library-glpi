06f00efeac88bd85a66b9fe99149725eeaf84c34
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-05 14:45:59 -0400

    ci(release): create first release
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/build_develop.sh
+++ ci/scripts/build_develop.sh
@@ -4,13 +4,14 @@ git checkout "$CIRCLE_BRANCH"
 GIT_COMMIT_DESC=$(git log --pretty=oneline -n 1 $CIRCLE_SHA1)
 yarn release -t '' --prerelease beta
 GIT_TAG=$(jq -r ".version" package.json)
+git reset HEAD^1
 
 yarn build 
 
 if [[ "$GIT_COMMIT_DESC" != *"build(npm): update library to version "* ]]
 then
-    git add lib/
-    git commit -m "build(npm): update library to version ${GIT_TAG}" --amend
+    git add lib/ package.json
+    git commit -m "build(npm): update library to version ${GIT_TAG}"
     git push origin "$CIRCLE_BRANCH"
     npm publish --tag beta --access public
 fi
