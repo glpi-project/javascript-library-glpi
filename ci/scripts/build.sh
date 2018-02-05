c472272c8f75a2c84fb1df4a1ccd356d49398e1c
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-05 14:45:59 -0400

    ci(release): create first release
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+TAG=$(jq -r ".version" package.json)
+
+if [ "$TAG" = "0.0.0" ] 
+then
+    yarn release -t '' --first-release
+fi
+
 if [ "$CIRCLE_BRANCH" = "develop" ] 
 then
     source ci/scripts/build_develop.sh
