9cad5efe7fbd7590de999073dba0990f111af700
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-02-08 18:40:11 -0400

    refactor(prepareRequest): change Request object to a simple object
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- src/prepareRequest.js
+++ src/prepareRequest.js
@@ -3,16 +3,17 @@ import createQueryString from './createQueryString'
 
 function prepareRequest (data) {
 
-    let myHeaders = new Headers()
-    let myInit = {}
+    let headers = {'Content-Type': 'application/json'}
+    let method = 'GET'
     let url = config.url
     let queryString = null
     let body = {}    
 
-    myHeaders.append('Content-Type', 'application/json')
-
     if (config.appToken) {
-        myHeaders.append('App-Token', config.appToken)
+        headers = {
+            ...headers,
+            'App-Token': config.appToken
+        }
     }
                          
     if (data.queryString) {
@@ -22,90 +23,77 @@ function prepareRequest (data) {
     switch (data.function) {
 
         case 'initSessionByCredentials': 
-            myHeaders.append('Authorization', `Basic ${Buffer.from(`${data.userName}:${data.userPassword}`).toString('base64')}`)
+            headers = {
+                ...headers,
+                'Authorization': `Basic ${Buffer.from(`${data.userName}:${data.userPassword}`).toString('base64')}`
+            }
             url = `${url}/initSession`
-            myInit = { method: 'GET' }
         break
 
         case 'initSessionByUserToken': 
-            myHeaders.append('Authorization', `user_token ${data.userToken}`)
-            
+            headers = {
+                ...headers,
+                'Authorization': `user_token ${data.userToken}`
+            }     
             url = `${url}/initSession`
-            myInit = { method: 'GET' }
         break
 
         case 'killSession': 
             url = `${url}/killSession`
-            myInit = { method: 'GET' }
         break
 
         case 'addItem':
             url = `${url}/${data.itemtype}`
-            myInit = { 
-                method: 'POST',
-                body: JSON.stringify({input: data.input})
-            }            
+            method = 'POST'
+            body = JSON.stringify({input: data.input})
         break
 
         case 'getFullSession':
             url = `${url}/getFullSession`
-            myInit = { method: 'GET'}            
         break
 
         case 'getActiveProfile':
             url = `${url}/getActiveProfile`
-            myInit = { method: 'GET'}            
         break
 
         case 'getMyProfiles':
             url = `${url}/getMyProfiles`
-            myInit = { method: 'GET'}            
         break
 
         case 'getMyEntities':
             url = `${url}/getMyEntities`
-            myInit = { method: 'GET'}            
         break
 
         case 'getActiveEntities':
             url = `${url}/getActiveEntities`
-            myInit = { method: 'GET'}            
         break
 
         case 'getGlpiConfig':
             url = `${url}/getGlpiConfig`
-            myInit = { method: 'GET'}            
         break
         
         case 'getAllItems': 
             url = `${url}/${data.itemtype}/${queryString ? queryString : ''}`
-            myInit = { method: 'GET' } 
         break
 
         case 'getAnItem': 
             url = `${url}/${data.itemtype}/${data.id}${queryString ? queryString : ''}`
-            myInit = { method: 'GET' } 
         break
 
         case 'getSubItems': 
             url = `${url}/${data.itemtype}/${data.id}/${data.subItemtype}${queryString ? queryString : ''}`
-            myInit = { method: 'GET' } 
         break
 
         case 'deleteItem': 
             url = `${url}/${data.itemtype}/${data.id ? data.id : ''}${ queryString ? queryString : '' }`
-            myInit = { 
-                method: 'DELETE',
-                body: JSON.stringify({input: data.input})
-            } 
+            method = 'DELETE'
+            body = JSON.stringify({input: data.input})
         break
         
         case 'updateItem':
             url = `${url}/${data.itemtype}/${data.id ? data.id : ''}`
-            myInit = { 
-                method: 'PUT',
-                body: JSON.stringify({input: data.input})
-            } 
+            method = 'PUT'
+            body = JSON.stringify({input: data.input})
         break
         
         case 'changeActiveEntities':
@@ -113,42 +101,29 @@ function prepareRequest (data) {
             body = {}
             if (data.entitiesId) body = {"entities_id": data.entitiesId}
             if (data.isRecursive) body = {...body, "is_recursive": data.isRecursive}
-            myInit = { 
-                method: 'POST',
-                body: JSON.stringify(body)
-            } 
+            method = 'POST'
+            body = JSON.stringify(body)
         break
 
         case 'changeActiveProfile':
             url = `${url}/changeActiveProfile/`
-            body = {}
-            if (data.profilesId) body = {"profiles_id": data.profilesId}
-            myInit = { 
-                method: 'POST',
-                body: JSON.stringify(body)
-            } 
+            body = data.profilesId ? {"profiles_id": data.profilesId} : {}
+            method = 'POST'
+            body = JSON.stringify(body)
         break
 
         case 'getMultipleItems':
             queryString = `?${createQueryString({items: data.items})}${createQueryString({items: data.options})}`
-
             url = `${url}/getMultipleItems${queryString}`
-
-            myInit = { method: 'GET' } 
         break
 
         case 'searchItems':
             queryString = `?${createQueryString({criteria: data.criteria})}${createQueryString({metacriteria: data.metacriteria})}${createQueryString({items: data.options})}`
-
             url = `${url}/search/${data.itemtype}/${queryString}`
-
-            myInit = { method: 'GET' } 
         break
 
         case 'listSearchOptions':
             url = `${url}/listSearchOptions/${data.itemtype}/${ queryString ? queryString : '' }`
-
-            myInit = { method: 'GET' } 
         break
 
         case 'genericRequest':
@@ -165,13 +140,8 @@ function prepareRequest (data) {
     if (config.sessionToken) {
         url = `${url}${queryString ? '' : '?'}session_token=${config.sessionToken}`        
     }
-
-    myInit = {
-        ...myInit,
-        headers: myInit.headers ? myInit.headers : myHeaders
-    }
     
-    return new Request(url, myInit)
+    return {url, headers, method, body}
 }
 
 export default prepareRequest
